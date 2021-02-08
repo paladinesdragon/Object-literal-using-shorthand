6950825ad398107cc13e204ef7573b663324aa4a
blackRabbit3dx <blackrabbit3dx@gmail.com>
2021-02-08 17:29:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//rewrite using ES6 to define object literals
+
+const createPerson = (name, age, gender) => {
+  return {
+    name: name,
+    age: age,
+    gender: gender
+  };
+};
+
+const createPerson2 = (name, age, gender) => ({name, age, gender});
+
+//console.log(createPerson('me', 34, 'male'));
+console.log(createPerson2('myself', 22, 'female'));
